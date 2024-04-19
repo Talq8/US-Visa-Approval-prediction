c648955783e1e2241907de45252a443ea74ccfea
User <talpythonq8@gmail.com>
2024-04-19 18:48:58 +0300

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Talal",
+    author_email="talpythonq8@gmail.com",
+    packages=find_packages()
+
+)
