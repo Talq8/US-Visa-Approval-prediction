0f0dd5fbe881a956fbc4f8f631b760fab25f6647
Talq8 <talpythonq8@gmail.com>
2024-04-22 18:25:50 +0300

    add logging to exception

--- demo_exception.py
+++ demo_exception.py
@@ -1,7 +1,9 @@
 from US_Visa.exception import USvisaException
+from US_Visa.logger import logging
 import sys
 
 try:
     a=1/"10"
 except Exception as e:
+    logging.info(e)
     raise USvisaException(e,sys) from e
