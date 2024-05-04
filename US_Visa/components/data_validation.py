dc1f4d9b7e48eb8a57b449b5c23e34481af05d41
Talq8 <talpythonq8@gmail.com>
2024-05-04 22:14:27 +0300

    updated

--- US_Visa/components/data_validation.py
+++ US_Visa/components/data_validation.py
@@ -7,12 +7,12 @@ from evidently.model_profile.sections import DataDriftProfileSection
 
 from pandas import DataFrame
 
-from us_visa.exception import USvisaException
-from us_visa.logger import logging
-from us_visa.utils.main_utils import read_yaml_file, write_yaml_file
-from us_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
-from us_visa.entity.config_entity import DataValidationConfig
-from us_visa.constants import SCHEMA_FILE_PATH
+from US_Visa.exception import USvisaException
+from US_Visa.logger import logging
+from US_Visa.utils.main_utils import read_yaml_file, write_yaml_file
+from US_Visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from US_Visa.entity.config_entity import DataValidationConfig
+from US_Visa.constants import SCHEMA_FILE_PATH
 
 
 class DataValidation:
